2160b436ba1ca5b1632f87c5475463350612b502
David Keyes <dgkeyes@gmail.com>
2019-04-12 11:35:17 -0700

    Add codebook

--- R/get-data.R
+++ R/get-data.R
@@ -114,3 +114,58 @@ nhanes_selected_vars <- NHANES %>%
 
 write_csv(nhanes_selected_vars, here("data", "nhanes.csv"))
 
+# Make codebook
+
+nhanes_vars <- nhanes_selected_vars %>% 
+  names()
+
+nhanes_vars_clean_names <- nhanes_selected_vars %>% 
+  clean_names() %>% 
+  names()
+
+nhanes_codebook <- 
+  tibble(variable_name = nhanes_vars,
+         variable_name_clean = nhanes_vars_clean_names) %>% 
+  mutate(description = c(
+    "Participant identifier.",
+    "Which survey the participant participated in.",
+    "Gender (sex) of study participant coded as male or female",
+    "Age in years at screening of study participant. Note: Subjects 80 years or older were recorded
+as 80.",
+    "Categorical variable derived from age with levels 0-9, 10-19, . . . 70+",
+    "Reported race of study participant: Mexican, Hispanic, White, Black, or Other",
+    "Educational level of study participant Reported for participants aged 20 years or older.
+One of 8thGrade, 9-11thGrade, HighSchool, SomeCollege, or CollegeGrad.",
+    "Marital status of study participant. Reported for participants aged 20 years or older.
+One of Married, Widowed, Divorced, Separated, NeverMarried, or LivePartner (living
+with partner).",
+    "Total annual gross income for the household in US dollars. One of 0 - 4999,
+5000 - 9,999, 10000 - 14999, 15000 - 19999, 20000 - 24,999, 25000 - 34999,
+    35000 - 44999, 45000 - 54999, 55000 - 64999, 65000 - 74999, 75000 - 99999, or
+    100000 or More.",
+    "One of Home, Rent, or Other indicating whether the home of study participant or someone in their family is owned, rented or occupied by some other arrangement.",
+    "Self-reported work status for participants aged 16 years or older. One of Working, NotWorking or Looking.",
+    "Weight in kg",
+    "Standing height in cm. Reported for participants aged 2 years or older",
+    "Body mass index (weight/height2 in kg/m2). Reported for participants aged 2 years or older",
+    "Self-reported rating of participant’s health in general Reported for participants aged 12
+years or older. One of Excellent, Vgood, Good, Fair, or Poor.",
+    "Self-reported number of days participant’s physical health was not good out of
+the past 30 days. Reported for participants aged 12 years or older.",
+    "Self-reported number of days participant’s mental health was not good out of
+the past 30 days. Reported for participants aged 12 years or older",
+    "Self-reported number of hours study participant usually gets at night on weekdays
+or workdays. Reported for participants aged 16 years and older",
+    "Participant does moderate or vigorous-intensity sports, fitness or recreational activities
+(Yes or No). Reported for participants 12 years or older.",
+    "Number of days in a typical week that participant does moderate or vigorousintensity activity. Reported for participants 12 years or older.",
+    "Number of hours per day on average participant watched TV over the past 30 days. Reported for participants 2 years or older. One of 0_to_1hr, 1_hr, 2_hr, 3_hr, 4_hr, More_4_hr.
+Not available 2009-2010.",
+    "Study participant currently smokes cigarettes regularly. Reported for participants aged
+20 years or older as Yes or No, provided they answered Yes to having somked 100 or more
+cigarettes in their life time. All subjects who have not smoked 100 or more cigarettes are listed
+as NA here."
+  ))
+
+
+write_csv(nhanes_codebook, here("data", "nhanes-codebook.csv"))
